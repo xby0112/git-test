5ec182b9992b55d98b49dc39090090dc4b1b4f74
xby <870184063@qq.com>
2020-04-01 09:59:00 +0800

    把本地工程添加到git

---
+++ test/src/com/bw/git/TestGit.java
@@ -0,0 +1,10 @@
+package com.bw.git;
+
+public class TestGit {
+    public static void main(String[] args) {
+        System.out.println("hello xby");
+        System.out.println("hello xby");
+        System.out.println("hello xby");
+        System.out.println("hello xby");
+    }
+}
